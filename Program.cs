1026c17643c717f65233ceff044c056999bcf469
arthur_ceschini <arthurceschini2009@gmail.com>
2026-06-15 20:02:26 -0300

    q14

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿Console.Write("Informe o número da tabuada: ");
-int numero = int.Parse(Console.ReadLine());
+﻿double soma = 0;
 
-for (int i = 1; i <= 10; i++)
-{
-    Console.WriteLine($"{numero} x {i} = {numero * i}");
-}
+Console.Write("Informe o primeiro valor: ");
+soma += double.Parse(Console.ReadLine());
+
+Console.Write("Informe o segundo valor: ");
+soma += double.Parse(Console.ReadLine());
+
+Console.Write("Informe o terceiro valor: ");
+soma += double.Parse(Console.ReadLine());
+
+Console.Write("Informe o quarto valor: ");
+soma += double.Parse(Console.ReadLine());
+
+Console.Write("Informe o quinto valor: ");
+soma += double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Soma dos valores: {soma}");
+Console.WriteLine($"Média dos valores: {soma / 5}");
